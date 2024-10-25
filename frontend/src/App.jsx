836a6507e002a52b0fbf006bd2e9128e04ba6ca6
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-25 09:35:17 -0300

    Updated config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
+import Test from "./pages/Test";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App.jsx";
 import "./index.css";
+import { Center } from "./components/Center.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
