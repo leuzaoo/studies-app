53d99473323fe6ca6d42114bedf897417f56c0e3
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-28 17:21:27 -0300

    Updated user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,6 +23,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    about: {
+      default: "",
+      type: String,
+      trim: true,
+    },
   },
   {
     timestamps: true,
