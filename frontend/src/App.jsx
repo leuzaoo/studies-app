8ce764200620867b1572733370e1994bb7a48e9d
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-28 11:51:44 -0300

    Added new lib, removed useless page and updated app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,62 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
 
+import LoadingSpinner from "./components/LoadingSpiner";
+import { useAuthStore } from "./store/authStore";
 import Homepage from "./pages/Homepage";
 import Signup from "./pages/Signup";
-import Test from "./pages/Test";
 
-export default function App() {
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/signup"} replace />;
+  }
+
+  return children;
+};
+
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { isAuthenticated } = useAuthStore();
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} replace />;
+  }
+
+  return children;
+};
+
+function App() {
+  const { isCheckingAuth, authCheck } = useAuthStore();
+
+  useEffect(() => {
+    authCheck();
+  }, [authCheck]);
+
+  if (isCheckingAuth) return <LoadingSpinner />;
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/test" element={<Test />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Homepage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <RedirectAuthenticatedUser>
+              <Signup />
+            </RedirectAuthenticatedUser>
+          }
+        />
       </Routes>
     </>
   );
 }
+
+export default App;
