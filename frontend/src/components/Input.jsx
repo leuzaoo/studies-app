b4ebcfa19a56f0ad9f6cfd6e6283946208263db4
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-28 11:04:26 -0300

    Updated component and update frontend code to user makes login

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Input = ({ type, placeholder }) => {
+const Input = ({ type, placeholder, value, onChange }) => {
   return (
     <input
+      value={value}
+      onChange={onChange}
       type={type}
       placeholder={placeholder}
       className="w-full border border-secondary-grey rounded-xl pl-5 h-[40px] text-sm outline-none focus:border-primary-dark"
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import { XCircle } from "lucide-react";
+
+import { useAuthStore } from "../store/authStore";
 import Button from "../components/Button";
 import Center from "../components/Center";
 import Input from "../components/Input";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const { signup, error, isLoading } = useAuthStore();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      await signup(name, username, email, password);
+      navigate("/");
+    } catch (error) {}
+  };
+
   return (
     <Center>
       <section>
@@ -10,13 +33,41 @@ const Signup = () => {
           Crie agora sua conta!
         </h1>
       </section>
-      <form className="mt-10 flex flex-col">
+      <form onSubmit={handleSignUp} className="mt-10 flex flex-col">
         <div className="space-y-3">
-          <Input type="text" placeholder="Nome e sobrenome" />
-          <Input type="text" placeholder="Nome de usuário" />
-          <Input type="email" placeholder="Email" />
-          <Input type="password" placeholder="Senha" />
+          <Input
+            type="text"
+            placeholder="Nome e sobrenome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder="Nome de usuário"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
+
+        {error && (
+          <div className="flex items-center gap-2 text-sm mb-6">
+            <XCircle className="text-red-600 size-5" />
+            <p className="text-red-600 font-semibold">{error}</p>
+          </div>
+        )}
+
         <div className="mt-5 text-center">
           <Button primary content={"Criar conta"} />
           <p className="text-primary-dark font-medium text-sm my-2 text-center">
