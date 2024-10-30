6da6c9f6c5c2bb38e1f245033942814b7778a9a8
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-30 14:51:11 -0300

    Added framer motion animation to app

--- frontend/src/pages/AboutMe.jsx
+++ frontend/src/pages/AboutMe.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 import { useAuthStore } from "../store/authStore";
 import { ToastContainer } from "react-toastify";
@@ -76,7 +77,7 @@ const AboutMe = () => {
                   // onChange={handleProfilePictureChange}
                 />
                 <label
-                  className="text-sm cursor-pointer bg-cyan-600 text-primary-bg px-2 py-2 rounded-lg"
+                  className="text-sm cursor-pointer bg-cyan-600 text-primary-bg px-2 py-2 rounded-lg hover:bg-cyan-500 transition-all duration-200"
                   htmlFor="fotoPerfil"
                 >
                   Alterar imagem
@@ -84,7 +85,7 @@ const AboutMe = () => {
               </div>
               <button
                 type="button"
-                className="text-sm cursor-pointer bg-red-100 text-red-600 px-2 py-2 rounded-lg"
+                className="text-sm cursor-pointer bg-red-100 text-red-600 px-2 py-2 rounded-lg hover:bg-red-200 transition-all duration-200"
                 onClick={() => setProfilePicture(null)}
               >
                 Remover imagem
@@ -136,13 +137,15 @@ const AboutMe = () => {
           {message && <p className="text-green-500 mt-3">{message}</p>}
 
           <div className="w-full text-right mt-5">
-            <button
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
               className="text-sm h-8 max-w-max px-3 rounded-lg bg-cyan-600 text-primary-bg"
               type="submit"
               disabled={isLoading}
             >
               {isLoading ? "Salvando..." : "Salvar alterações"}
-            </button>
+            </motion.button>
           </div>
         </form>
       </Center>
