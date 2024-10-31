e05270ef15fc7d0b2092f6153f1b8a864bbf7614
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-31 12:20:39 -0300

    Added antd design to create select field

--- frontend/src/pages/NewStudy.jsx
+++ frontend/src/pages/NewStudy.jsx
@@ -1,43 +1,24 @@
 import { ToastContainer } from "react-toastify";
+import { useState } from "react";
 
-import { MenuDropdown, MenuItem } from "../components/MenuDropdown";
+import { Select } from "antd";
+
+import LabelFormTitle from "../components/LabelFormTitle";
 import TextEditor from "../components/TextEditor";
 import TitlePage from "../components/TitlePage";
 import Navbar from "../components/Navbar";
 import Center from "../components/Center";
 import Input from "../components/Input";
-import { useState } from "react";
-
-const menu = {
-  closed: {
-    scale: 0,
-    transition: {
-      delay: 0.15,
-    },
-  },
-
-  open: {
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.4,
-      delayChildren: 0.2,
-      staggerChildren: 0.05,
-    },
-  },
-};
-
-const item = {
-  variants: {
-    closed: { x: -16, opacity: 0 },
-    open: { x: 0, opacity: 1 },
-  },
-
-  transition: { opacity: { duration: 0.2 } },
-};
 
 const NewStudy = () => {
-  const [open, setOpen] = useState(false);
+  const [tags, setTags] = useState([]);
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  const handleTagsChange = (value) => {
+    setTags(value);
+    console.log("Tags:", value);
+  };
 
   return (
     <>
@@ -46,34 +27,45 @@ const NewStudy = () => {
       <div className="max-w-4xl mx-auto ">
         <Center>
           <TitlePage text={"Criar novo estudo"} />
-          <div className="mt-5">
-            <Input placeholder={"Título"} />
-            <MenuDropdown
-              label="Options"
-              open={open}
-              setOpen={setOpen}
-              animate={open ? "open" : "closed"}
-              initial="closed"
-              exit="closed"
-              variants={menu}
-            >
-              <MenuItem {...item}>Edit</MenuItem>
-              <MenuItem {...item}>Share</MenuItem>
-              <MenuItem {...item}>Delete</MenuItem>
-              <MenuItem {...item}>Report</MenuItem>
-            </MenuDropdown>
-          </div>
-          <section className="mt-5">
-            <TextEditor
-              label="Options"
-              open={open}
-              setOpen={setOpen}
-              animate={open ? "open" : "closed"}
-              initial="closed"
-              exit="closed"
-              variants={menu}
+          <form className="mt-5 flex flex-col">
+            <LabelFormTitle text={"Título"} />
+            <Input
+              className={"mb-3"}
+              placeholder={"Você sabe tudo sobre o nazismo?"}
             />
-          </section>
+
+            <LabelFormTitle text={"Categoria"} />
+            <Select
+              defaultValue=""
+              style={{
+                width: 160,
+                marginBottom: 12,
+              }}
+              onChange={handleChange}
+              options={[
+                { value: "política", label: "Política" },
+                { value: "esportes", label: "Esportes" },
+                { value: "programação", label: "Programação" },
+                { value: "ciência", label: "Ciência" },
+                { value: "história", label: "História" },
+                { value: "arte", label: "Arte" },
+                { value: "outros", label: "Outros" },
+              ]}
+            />
+
+            <LabelFormTitle text={"Tags"} />
+            <Select
+              showArrow={false}
+              mode="tags"
+              style={{ width: "100%", marginBottom: 12 }}
+              placeholder="Guerra, Nazismo, Judeus"
+              onChange={handleTagsChange}
+              value={tags}
+              notFoundContent={null}
+            />
+
+            <TextEditor />
+          </form>
         </Center>
       </div>
     </>
