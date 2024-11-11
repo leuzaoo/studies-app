61d9216238d1b8b66d497a23ee38f2e9125d4901
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-11-11 10:34:45 -0300

    Created component to render menu items in navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,19 +1,10 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import { useState } from "react";
-import { Menu } from "antd";
 
 import { useAuthStore } from "../../store/authStore";
 
-import {
-  LogOut,
-  LucideMenu,
-  NotebookIcon,
-  NotebookPen,
-  Settings2Icon,
-  User,
-  X,
-} from "lucide-react";
+import { LucideMenu, X } from "lucide-react";
+import MenuItems from "./MenuItems";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,8 +13,6 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-
-  const { logout } = useAuthStore();
 
   return (
     <>
@@ -57,54 +46,7 @@ const Navbar = () => {
             </button>
           </div>
 
-          <div
-            className={`fixed top-0 right-0 h-full bg-white shadow-lg w-4/5 max-w-xs transition-transform duration-300 ease-in-out z-40 ${
-              isMenuOpen ? "translate-x-0" : "translate-x-full"
-            }`}
-          >
-            <ul className="flex flex-col justify-between h-full p-6">
-              <div className="flex flex-col space-y-6 font-light">
-                <Link
-                  to={"/new-study"}
-                  className="flex items-center gap-3 text-lg hover:text-gray-500 cursor-pointer"
-                >
-                  <NotebookPen strokeWidth={1} />
-                  Novo estudo
-                </Link>
-                <Link
-                  to={"/about-me"}
-                  className="flex items-center gap-3 text-lg hover:text-gray-500 cursor-pointer"
-                >
-                  <User strokeWidth={1} />
-                  Minha conta
-                </Link>
-                <Link to={"/my-studies"}>
-                  <li className="flex items-center gap-3 text-lg hover:text-gray-500 cursor-pointer">
-                    <NotebookIcon strokeWidth={1} />
-                    Meus estudos
-                  </li>
-                </Link>
-                <li className="flex items-center gap-3 text-lg hover:text-gray-500 cursor-pointer">
-                  <Settings2Icon strokeWidth={1} />
-                  Configurações
-                </li>
-              </div>
-              <motion.button
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                onClick={logout}
-                className="flex items-center gap-2 bg-primary-orange max-w-max px-3 py-1 rounded-lg mx-auto text-white font-light cursor-pointer"
-              >
-                Sair
-                <LogOut
-                  size={16}
-                  className="bg-transparent"
-                  color="white"
-                  strokeWidth={2}
-                />
-              </motion.button>
-            </ul>
-          </div>
+          <MenuItems isMenuOpen={isMenuOpen} />
 
           {isMenuOpen && (
             <div
