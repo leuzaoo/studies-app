c1771b739d7bdc4ac1d7a1e555f2fd813332533b
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-11-21 10:27:17 -0300

    Removed useless lines

--- backend/routes/comments.route.js
+++ backend/routes/comments.route.js
@@ -4,12 +4,10 @@ import verifyToken from "../middleware/verifyToken.js";
 import {
   newComment,
   getStudyCommentsById,
-  getStudyCommentsCount,
 } from "../controllers/comments.controller.js";
 
 const router = express.Router();
 
-router.get("/:id/comments-count", getStudyCommentsCount);
 router.get("/study/:id/comments", getStudyCommentsById);
 
 router.post("/study/:id/new-comment", verifyToken, newComment);
