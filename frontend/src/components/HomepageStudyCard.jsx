201415736b6a887a95512187ea79820e96ae46a3
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-11-22 18:06:36 -0300

    Updated frontend like button

--- frontend/src/components/HomepageStudyCard.jsx
+++ frontend/src/components/HomepageStudyCard.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
-
+import { useMetricsStore } from "../store/metricsStore";
+import { MessageCircle, Heart } from "lucide-react";
 import { formatDate } from "../utils/formatDate.js";
-import { MessageCircle } from "lucide-react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const HomepageStudyCard = ({
   title,
@@ -12,7 +13,31 @@ const HomepageStudyCard = ({
   username,
   createdAt,
   commentsCount,
+  likesCount,
+  hasLiked,
 }) => {
+  const toggleStudyLike = useMetricsStore((state) => state.toggleStudyLike);
+  const [likes, setLikes] = useState(likesCount || 0);
+  const [liked, setLiked] = useState(hasLiked || false);
+
+  const handleLike = async () => {
+    const newLikedState = !liked;
+    const newLikesCount = newLikedState ? likes + 1 : likes - 1;
+
+    setLiked(newLikedState);
+    setLikes(newLikesCount);
+
+    try {
+      const response = await toggleStudyLike(_id);
+      setLikes(response.likes);
+      setLiked(response.hasLiked);
+    } catch (error) {
+      setLiked(!newLikedState);
+      setLikes(newLikedState ? likes - 1 : likes + 1);
+      console.error("Erro ao alternar like:", error);
+    }
+  };
+
   return (
     <>
       <Link
@@ -20,7 +45,7 @@ const HomepageStudyCard = ({
         className="flex gap-10 items-center justify-between py-3"
       >
         <div className="flex flex-col justify-between gap-3 max-w-[520px]">
-          <h2 className="font-semibold  md:text-2xl leading-none">{title}</h2>
+          <h2 className="font-semibold md:text-2xl leading-none">{title}</h2>
           <p className="font-domine leading-none text-[12px] md:text-base font-light">
             {description.length > 60
               ? description.substring(0, 60) + "..."
@@ -36,9 +61,13 @@ const HomepageStudyCard = ({
               <span className="ml-2">{formatDate(createdAt)}</span>
             </p>
           </div>
-          <div className="flex gap-1 items-center">
-            <MessageCircle strokeWidth={2} color="grey" size={16} />
-            <span className="text-sm text-terciary-grey font-light">{commentsCount}</span>
+          <div className="flex gap-3 items-center">
+            <div className="flex gap-1 items-center">
+              <MessageCircle strokeWidth={2} color="grey" size={16} />
+              <span className="text-sm text-terciary-grey font-light">
+                {commentsCount}
+              </span>
+            </div>
           </div>
         </div>
         <img
@@ -46,6 +75,21 @@ const HomepageStudyCard = ({
           className="object-cover min-w-[120px] max-w-[120px] h-[68px] md:max-w-[160px] md:min-w-[200px] md:h-[112px] rounded-xl shadow-lg"
         />
       </Link>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleLike();
+        }}
+        className="flex gap-1 items-center"
+      >
+        <Heart
+          strokeWidth={2}
+          color={liked ? "red" : "grey"}
+          fill={liked ? "red" : "none"}
+          size={16}
+        />
+        <span className="text-sm text-terciary-grey font-light">{likes}</span>
+      </button>
     </>
   );
 };
